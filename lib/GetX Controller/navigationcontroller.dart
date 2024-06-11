dbbd4945994541a9ce584479cef2a88e54a8df41
RxDesss <rxdesss.com>
2024-06-11 10:16:43 +0530

    login page navigation error and home page navigation error solved

--- lib/GetX Controller/navigationcontroller.dart
+++ lib/GetX Controller/navigationcontroller.dart
@@ -2,16 +2,17 @@ import 'package:demo_project/Screens/cartScreen.dart';
 import 'package:demo_project/Screens/homeScreen.dart';
 import 'package:demo_project/Screens/profileScreen.dart';
 import 'package:get/get.dart';
-import 'package:get/state_manager.dart';
 
-
-class NavigationController extends GetxController{
-  final Rx<int> selectedIndex=0.obs;
-  final screens=[
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
     const HomeScreen(),
     const CartScreeen(),
-    const ProfileScreen()
+    const ProfileScreen(),
   ];
+
+  // History to keep track of the tab navigation
+  final RxList<int> history = <int>[0].obs;
 
   void clear() {}
 }
--- lib/Screens/loginScreen.dart
+++ lib/Screens/loginScreen.dart
@@ -23,119 +23,143 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text('Do you want to exit the login page?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.92,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.12,
-                      width: MediaQuery.of(context).size.width * 0.25,
-                      child: Image.asset(
-                        'assets/TexasImage.png',
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.05,
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(
-                        right: MediaQuery.of(context).size.width * 0.05,
-                        left: MediaQuery.of(context).size.width * 0.05,
-                      ),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              initialValue: 'dev@desss.com',
-                              onSaved: (value) {
-                                username = value;
-                              },
-                              decoration:
-                                  const InputDecoration(hintText: "Email"),
-                              validator: (value) {
-                                String pattern =
-                                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
-                                RegExp regExp = RegExp(pattern);
-                                if (value == null || value.isEmpty) {
-                                  return "Enter Email Id";
-                                } else if (!regExp.hasMatch(value)) {
-                                  return 'Enter a valid email address';
-                                }
-                                return null;
-                              },
-                            ),
-                            TextFormField(
-                              initialValue: '\$heshU98',
-                              onSaved: (value) {
-                                password = value;
-                              },
-                              obscureText: true,
-                              validator: (value) {
-                                if (value == null || value == "" ) {
-                                  return "Enter a password";
-                                }else if (value.length < 5){
-                                  return "Enter a valid password";
-                                }
-                                return null;
-                              },
-                              decoration:
-                                  const InputDecoration(hintText: "Password"),
-                            ),
-                          ],
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.92,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.12,
+                        width: MediaQuery.of(context).size.width * 0.25,
+                        child: Image.asset(
+                          'assets/TexasImage.png',
+                          fit: BoxFit.fill,
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.04,
-                    ),
-                    Obx(
-                      () => Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color(0xFFCC0000),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                          right: MediaQuery.of(context).size.width * 0.05,
+                          left: MediaQuery.of(context).size.width * 0.05,
                         ),
-                        width: MediaQuery.of(context).size.width * 0.85,
-                        child: TextButton(
-                          onPressed: logincontroller.isLoading.value
-                              ? null
-                              : () {
-                                  _saveForm(context);
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                initialValue: 'dev@desss.com',
+                                onSaved: (value) {
+                                  username = value;
                                 },
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
+                                decoration:
+                                    const InputDecoration(hintText: "Email"),
+                                validator: (value) {
+                                  String pattern =
+                                      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+                                  RegExp regExp = RegExp(pattern);
+                                  if (value == null || value.isEmpty) {
+                                    return "Enter Email Id";
+                                  } else if (!regExp.hasMatch(value)) {
+                                    return 'Enter a valid email address';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              TextFormField(
+                                initialValue: '\$heshU98',
+                                onSaved: (value) {
+                                  password = value;
+                                },
+                                obscureText: true,
+                                validator: (value) {
+                                  if (value == null || value == "") {
+                                    return "Enter a password";
+                                  } else if (value.length < 5) {
+                                    return "Enter a valid password";
+                                  }
+                                  return null;
+                                },
+                                decoration:
+                                    const InputDecoration(hintText: "Password"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.04,
+                      ),
+                      Obx(
+                        () => Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: const Color(0xFFCC0000),
+                          ),
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          child: TextButton(
+                            onPressed: logincontroller.isLoading.value
+                                ? null
+                                : () {
+                                    _saveForm(context);
+                                  },
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    // SizedBox(
-                    //   height: MediaQuery.of(context).size.height * 0.04,
-                    // ),
-                    // const Text("Don't have an account "),
-                    // TextButton(
-                    //   onPressed: () {
-                    //     Get.to(() => const RegisterScreen());
-                    //   },
-                    //   child: const Text("Sign Up"),
-                    // ),
-                  ],
+                      // SizedBox(
+                      //   height: MediaQuery.of(context).size.height * 0.04,
+                      // ),
+                      // const Text("Don't have an account "),
+                      // TextButton(
+                      //   onPressed: () {
+                      //     Get.to(() => const RegisterScreen());
+                      //   },
+                      //   child: const Text("Sign Up"),
+                      // ),
+                    ],
+                  ),
                 ),
               ),
             ),
